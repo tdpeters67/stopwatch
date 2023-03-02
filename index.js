c6f27a39a3217f81f79895b317b321d593398707
Tomas Peters <tdpeters6794@gmail.com>
2023-03-02 18:42:31 -0500

    made event listeners for all three buttons with basic functions

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ let intervalId;
 let hrs = 0
 let mins = 0
 let secs = 0
+
+
+startBtn.addEventListener("click", () =>{
+    if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime
+        intervalId = setInterval(updateTime, 1000)
+    }
+})
+pauseBtn.addEventListener("click", () =>{
+    if(!paused)
+    paused = true
+    elapsedTime = Date.now() - startTime;
+    clearInterval(intervalId)
+})
+resetBtn.addEventListener("click", () =>{
+    paused = true
+    clearInterval(intervalId)
+    startTime = 0
+    elapsedTime = 0
+    currentTime = 0
+    hrs = 0
+    mins = 0
+    secs = 0    
+    timeDisplay.textContent = "00:00:00"
+
+})
+
